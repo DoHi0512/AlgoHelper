81a838893e6f2a4baf702f346a18544e8106afe0
DoHi0512 <donghuni512@gmail.com>
2022-08-31 20:27:59 +0900

    Interval 테스트

--- src/App.js
+++ src/App.js
@@ -11,26 +11,18 @@ import Stat from "./pages/stat";
 
 function App() {
   const [token, setToken] = useState(null);
-  const [boj,setBoj] = useState(); 
-  async function pullProblem() {
-    const form = {
-      url: `https://solved.ac/search?query=*b5..r1+%21%40${boj}&sort=random&direction=desc&page=1`,
-    };
-    const response = axios.post("http://127.0.0.1:8000/ahapp/problem/", form);
-    console.log((await response).data);
-    localStorage.problem = JSON.stringify((await response).data)
-  }
+  const [boj, setBoj] = useState();
+
   async function pullId() {
     const form = { name: localStorage.getItem("username") };
     const response = axios.post("http://127.0.0.1:8000/ahapp/getboj/", form);
-    setBoj(((await response).data));
+    setBoj((await response).data);
   }
   useEffect(() => {
     setToken(localStorage.getItem("token"));
-    pullId()
-    pullProblem();
-
+    pullId();
   }, []);
+
   return (
     <>
       <Header token={token} />
@@ -38,7 +30,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
-        <Route exact path="/recommend" element={<Recommend />} />
+        <Route exact path="/recommend" element={<Recommend boj={boj} />} />
         <Route exact path="/stat" element={<Stat />} />
       </Routes>
     </>
--- src/pages/Recommend.js
+++ src/pages/Recommend.js
@@ -1,32 +1,50 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../static/Recommend.css";
-export default function Recommend() {
-  const problem = JSON.parse(localStorage.problem)
+export default function Recommend(props) {
+  async function pullProblem() {
+    const form = {
+      url: `https://solved.ac/search?query=*b5..r1+%21%40${props.boj}&sort=random&direction=desc&page=1`,
+    };
+    const response = axios.post("http://127.0.0.1:8000/ahapp/problem/", form);
+    setPloblem((await response).data);
+  }
+  const [problem, setPloblem] = useState([]);
   const [isChecked, setIsChecked] = useState(false);
   const [checkedItems, setCheckedItems] = useState(new Set());
+  useEffect(() => {
+    let timer = setInterval(() => {
+      pullProblem();
+    }, 10000);
+    return () => clearInterval(timer);
+  });
   const checkHandler = ({ target }) => {
-    setIsChecked(!isChecked)
+    setIsChecked(!isChecked);
     checkedItemHandler(target.value, target.checked);
-  }
+  };
   const checkedItemHandler = (id, isChecked) => {
     if (isChecked) {
-      checkedItems.add(id)
-      setCheckedItems(checkedItems)
+      checkedItems.add(id);
+      setCheckedItems(checkedItems);
     } else if (!isChecked && checkedItems.has(id)) {
-      checkedItems.delete(id)
-      setCheckedItems(checkedItems)
+      checkedItems.delete(id);
+      setCheckedItems(checkedItems);
     }
-    return checkedItems
-  }
-  const problemTable = problem.map((data,idx) => (
+    return checkedItems;
+  };
+  const problemTable = problem.map((data, idx) => (
     <div>
       <img src={data["img"]} />
-      <a href={data["url"]}
-        className={`${checkedItems.has(idx.toString()) ? "solve" : "non_solve"
-          }`}
-      >{data['problem']}</a>
-      <input className="inp"
+      <a
+        href={data["url"]}
+        className={`${
+          checkedItems.has(idx.toString()) ? "solve" : "non_solve"
+        }`}
+      >
+        {data["problem"]}
+      </a>
+      <input
+        className="inp"
         type="checkbox"
         value={idx}
         onChange={(e) => checkHandler(e)}
