ae0adba2c76ca0ea102a8d4949777fa03d3027e6
DoHi0512 <donghuni512@gmail.com>
2022-09-28 20:33:02 +0900

    feat : 검색 페이지 개발

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../static/Search.css";
+import { BiSearch } from "react-icons/bi";
+import axios from "axios";
 export default function Search() {
+  const [problems, setProblems] = useState([]);
+  let problemDiv;
+  useEffect(() => {
+    problemDiv = problems.map((data) => (
+      <div className="problem">
+        <a href={data.href}>{data.title}</a>
+      </div>
+    ));
+  }, [problems]);
+
+  function sub() {
+    const name = document.getElementById("q").value;
+    axios
+      .get("https://solved.ac/api/v3/search/suggestion", {
+        params: { query: name },
+      })
+      .then((res) => {
+        console.log(res.data.problems);
+        setProblems(res.data.problems);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <div className="search">
       <div className="searchbar">
-        <form>
-          <p>문제 검색</p>
-          <input type="search" />
-          <input type="submit" />
+        <form className="searchForm">
+          <input
+            type="search"
+            id="q"
+            className="searchInput"
+            placeholder="문제 검색"
+          />
+          <BiSearch className="searchicon" onClick={sub} />
         </form>
       </div>
+      {problemDiv}
     </div>
   );
 }
